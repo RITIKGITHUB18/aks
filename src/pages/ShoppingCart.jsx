c7b6f4e5677e58def18c1e25ad7feee94ec6a94c
RITIKGITHUB18 <ritikasus911@gmail.com>
2025-01-20 00:30:41 +0530

    Back navigation fixed.

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -60,12 +60,12 @@ const ShoppingCart = () => {
     <div className="w-full justify-center text-white flex flex-col">
       <div className="fixed top-0 z-50 bg-[#090D14] px-4 pb-4 w-full h-[180px] ml-4 sm:ml-10">
         <Link onClick={handleBack}>
-          <div className="rounded-full p-2 hover:bg-gray-600 w-[44px] h-[44px] bg-[#090D14] border border-[#202938] flex items-center justify-center translate-y-[50px]">
+          <div className="rounded-full p-2 hover:bg-gray-600 w-[44px] h-[44px] bg-[#090D14] border border-[#202938] flex items-center justify-center translate-y-[20px]">
             <img src={leftArrow} alt="Back" className="w-6 h-6" />
           </div>
         </Link>
 
-        <h1 className="flex mt-10 ml-2 sm:ml-8 text-[24px] font-[500] leading-[31.2px] translate-y-[50px]">
+        <h1 className="flex ml-2 sm:ml-8 text-[24px] font-[500] leading-[31.2px] translate-y-[50px]">
           Shopping Cart
         </h1>
       </div>
